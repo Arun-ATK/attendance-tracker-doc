9ee8a73c407e0d8e63c18581b5b211a7ff8eba24
Arun-ATK <61749361+Arun-ATK@users.noreply.github.com>
2021-05-27 22:24:38 +0530

    trigger for threshold updation

---
+++ SQL/FUNC_ISPRESENT_UPDATE.sql
@@ -0,0 +1,21 @@
+CREATE FUNCTION ispresent_update()
+RETURNS trigger AS $ispresent_update$
+DECLARE
+    new_threshold_val   INTEGER;
+
+BEGIN
+    new_threshold_val = NEW.DURATION * NEW.THRESHOLD_PERCENT;
+    new_threshold_val = new_threshold_val / 100;
+
+    IF(TG_OP = 'UPDATE') THEN
+        UPDATE ATTENDS
+        SET ISPRESENT = CASE 
+            WHEN DURATION >= new_threshold_val THEN TRUE
+            ELSE FALSE 
+        END
+        WHERE LECTURE_ID = NEW.LECTURE_ID;
+    END IF;
+
+    RETURN NULL;
+END;
+$ispresent_update$ LANGUAGE plpgsql;
