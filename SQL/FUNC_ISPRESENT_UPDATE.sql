dce2983bc6437e2aae0306409ce47c32ac94337d
Arun-ATK <61749361+Arun-ATK@users.noreply.github.com>
2021-05-31 08:08:24 +0530

    Added grace time in trigger function

--- SQL/FUNC_ISPRESENT_UPDATE.sql
+++ SQL/FUNC_ISPRESENT_UPDATE.sql
@@ -2,10 +2,11 @@ CREATE FUNCTION ispresent_update()
 RETURNS trigger AS $ispresent_update$
 DECLARE
     new_threshold_val   INTEGER;
+    GRACE_TIME          INTEGER := 5;
 
 BEGIN
     new_threshold_val = NEW.DURATION * NEW.THRESHOLD_PERCENT;
-    new_threshold_val = new_threshold_val / 100;
+    new_threshold_val = (new_threshold_val / 100) - GRACE_TIME;
 
     IF(TG_OP = 'UPDATE') THEN
         UPDATE ATTENDS
